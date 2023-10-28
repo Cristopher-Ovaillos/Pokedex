36a54b7d4bf1fa9dc34ac1715c022f6c1344d4c5
guillermo-diaz <guillermo.diaz@est.fi.uncoma.edu.ar>
2023-10-28 17:38:11 -0300

    fix rutas

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,49 @@ function load_header() {
     let header = document.createElement("header");
     header.className = "container-fluid";
 
+    // Define las rutas por defecto
+    let rutaIndex = "../index.html";
+    let rutaPokedex = "../html/pokedex.html";
+    let rutaBuild = "../html/build.html";
+    let rutaImg = "../img/pokemon logo.png";
+
+    // Verifica si el archivo que lo abrió fue "index.html"
+    if (document.location.pathname.endsWith("index.html")) {
+        // Cambia las rutas si es "index.html"
+        rutaIndex = "index.html";
+        rutaPokedex = "html/pokedex.html";
+        rutaBuild = "html/build.html";
+        rutaImg = "img/pokemon logo.png"
+    }
+
     header.innerHTML = `
-            <nav class="navbar navbar-expand-md">
-                <a class="navbar-brand">
-                    <img class="logo" src="/img/pokemon logo.png" alt="logo pokemon">
-                </a>
-    
-                <button class="navbar-toggler btn-collapse" data-bs-toggle="collapse" data-bs-target="#opcionesNav" aria-controls="opcionesNav" aria-expanded="false">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-    
-                <div class="collapse navbar-collapse" id="opcionesNav">
-                    <div class="nav">
-                        <a id="nav-inicio" href="/index.html">
-                            <span class="iconify icon" data-icon="ion:home-outline"></span>
-                            <span class="text">inicio</span>
-                        </a>
-                        <a id="nav-pokedex" href="/html/pokedex.html">
-                            <span class="iconify icon" data-icon="iconoir:pokeball"></span>
-                            <span class="text">Pokedex</span>
-                        </a>
-                        <a id="nav-build" href="/html/build.html">
-                            <span class="iconify icon" data-icon="material-symbols:swords"></span>
-                            <span class="text">Build</span>
-                        </a>
-                    </div>
+        <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand">
+                <img class="logo" src="${rutaImg}" alt="logo pokemon">
+            </a>
+
+            <button class="navbar-toggler btn-collapse" data-bs-toggle="collapse" data-bs-target="#opcionesNav" aria-controls="opcionesNav" aria-expanded="false">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="opcionesNav">
+                <div class="nav">
+                    <a id="nav-inicio" href="${rutaIndex}">
+                        <span class="iconify icon" data-icon="ion:home-outline"></span>
+                        <span class="text">inicio</span>
+                    </a>
+                    <a id="nav-pokedex" href="${rutaPokedex}">
+                        <span class="iconify icon" data-icon="iconoir:pokeball"></span>
+                        <span class="text">Pokedex</span>
+                    </a>
+                    <a id="nav-build" href="${rutaBuild}">
+                        <span class="iconify icon" data-icon="material-symbols:swords"></span>
+                        <span class="text">Build</span>
+                    </a>
                 </div>
-            </nav>
-        `;
+            </div>
+        </nav>
+    `;
 
     let body = document.body;
     body.insertBefore(header, body.firstChild);
@@ -38,4 +53,4 @@ function load_header() {
 
 document.addEventListener("DOMContentLoaded", () => {
     load_header();
-})
+});
