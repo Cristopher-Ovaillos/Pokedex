5f9656a7150604fb8b9885ec68d8da39ab4e7663
guillermo-diaz <guillermo.diaz@est.fi.uncoma.edu.ar>
2023-10-28 20:35:03 -0300

    /

--- js/inicio.js
+++ js/inicio.js
@@ -1,5 +1,5 @@
 function fixRutas(){
-    document.querySelector('img.logo').src = "/img/pokemon logo.png"; 
+    document.querySelector('img.logo').src = "img/pokemon logo.png"; 
     document.querySelector('#nav-inicio').href = "index.html";
     document.querySelector('#nav-pokedex').href = "html/pokedex.html";
     document.querySelector('#nav-build').href = "html/build.html";
