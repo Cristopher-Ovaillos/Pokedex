e16705f2ee1d0e9beade88d075414384d3c9705b
guillermo-diaz <guillermo.diaz@est.fi.uncoma.edu.ar>
2023-10-30 00:14:30 -0300

    change theme

--- js/inicio.js
+++ js/inicio.js
@@ -1,8 +1,10 @@
+//arreglo las rutas que se rompen con index.html
 function fixRutas(){
     document.querySelector('img.logo').src = "img/pokemon logo.png"; 
     document.querySelector('#nav-inicio').href = "index.html";
     document.querySelector('#nav-pokedex').href = "html/pokedex.html";
     document.querySelector('#nav-build').href = "html/build.html";
+    document.querySelector('#pokeball-button').style.backgroundImage = "img/pokeball.png";
 }
 
 document.addEventListener('DOMContentLoaded', () =>{
